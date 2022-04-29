864b38f5a1162dac6891b0821bf6dc497bdd486c
Rand <63405866+Rand-RB@users.noreply.github.com>
2022-04-30 02:38:31 +0300

    Update TB_StreamlitApp.py

--- TB_StreamlitApp.py
+++ TB_StreamlitApp.py
@@ -20,6 +20,7 @@ if  uploaded_img is not None:
 
     img_bytes = np.asarray(bytearray(uploaded_img.read()), dtype = np.uint8) # Convert to an opencv image.
     cv_Img = cv2.imdecode(img_bytes, 1)
+    img = cv2.imread(cv_Img, 0)
     gray_img  = cv2.cvtColor(cv_Img, cv2.COLOR_BGR2RGB)
     img_hist =cv2.equalizeHist(gray_img)
     clahe = cv2.createCLAHE(clipLimit=3).apply(img_hist)
