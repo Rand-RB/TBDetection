ca59a984e13fb456e0253f4ac7bf42d8dd3f5a65
Rand <63405866+Rand-RB@users.noreply.github.com>
2022-04-30 13:09:44 +0300

    Update TB_StreamlitApp.py

--- TB_StreamlitApp.py
+++ TB_StreamlitApp.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
+from tensorflow.keras.applications.vgg16 import preprocess_input
 import pickle
 import tensorflow as tf
 import cv2
