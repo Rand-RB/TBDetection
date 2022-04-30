4039ec0bae2cc786637c459d5725d507ee8215df
Rand <63405866+Rand-RB@users.noreply.github.com>
2022-04-30 14:14:54 +0300

    Update TB_StreamlitApp.py

--- TB_StreamlitApp.py
+++ TB_StreamlitApp.py
@@ -20,7 +20,7 @@ uploaded_img = st.file_uploader("Choose an Chest X-Ray Image the extention shoul
 if  uploaded_img is not None:
 
     img_bytes = np.asarray(bytearray(uploaded_img.read()), dtype = np.uint8) # Convert to an opencv image.
-    cv_Img = cv2.imdecode(img_bytes, 3)
+    cv_Img = cv2.imdecode(img_bytes, 1)
     #img = cv2.imread(cv_Img, 0)
     gray_img = cv2.cvtColor(cv_Img,cv2.COLOR_BGR2GRAY)
     img_hist =cv2.equalizeHist(gray_img)
@@ -31,12 +31,12 @@ if  uploaded_img is not None:
     
     
     #img = image.load_img(invert, target_size=(512, 512))
-    x = image.img_to_array(resized_img)
-    x = np.expand_dims(x, axis=0)
+    #x = image.img_to_array(resized_img)
+    #x = np.expand_dims(x, axis=0)
     #img = x.reshape(512,512,3)
     #x = preprocess_input(x, data_format=None)
     #st.image(x)
-    resized = mobilenet_v2_preprocess_input(x)
+    resized = mobilenet_v2_preprocess_input(resized_img)
     img_reshape = resized[np.newaxis,...]
 #input = tf.Tensor(shape=(32, 512,512,3))
     
